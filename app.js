dbbcfd3980683ccb91f32741d8df46d7df0ef374
MasseyIsaako <lasiisaako@gmail.com>
2017-08-15 11:52:13 +1200

    using get array to output specified json data

--- app.js
+++ app.js
@@ -20,10 +20,26 @@ app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
 app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
 app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
 
-app.get("/products", function(request, response){
+// Getting information from url bar
+app.get("/allProducts", function(request, response){
 	response.json(data);
 });
 
+// Getting stocked otems using a function
+app.get("/stocked", function(request, response){
+	instock(response);
+});
+
+// Getting items below 50
+app.get("/belowFifty", function(request, response){
+	belowFifty(response);
+});
+
+// Getting items above 50
+app.get("/aboveFifty", function(request, response){
+	aboveFifty(response);
+});
+// Calling the about page
 app.get("/about", function(request, response){
 	response.sendFile(path.join(__dirname + "/public/about.html"));
 });
@@ -32,3 +48,28 @@ app.use(cors);
 
 app.listen(3000);
 console.log("Server running on port 3000.");
+
+// Calling Functions
+function instock(response){
+	var available = data.filter(function(item){
+		return item.inStock === true;
+	});
+
+	response.json(available);
+}
+
+function belowFifty(response){
+	var cheaper = data.filter(function(item){
+		return item.price < 50;
+	});
+
+	response.json(cheaper);
+}
+
+function aboveFifty(response){
+	var costlier = data.filter(function(item){
+		return item.price >= 51;
+	});
+
+	response.json(costlier);
+}
