57501ba8b065f383722afb030648ba3d10b897da
MasseyIsaako <lasiisaako@gmail.com>
2017-08-16 12:35:17 +1200

    added search bar functionality
    user can now search for items in the grocery json file. This uses url
    get array and ajax to retrieve data from localhost server, and have it
    appended/updated live onscreen using js/jquery and html.

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ app.use(function(request, response, next){
 	next();
 });
 
-
+// Connecting all files inside public folder
 app.use(express.static("./public"));
+
+// Finding the path of modules using the path module.
 app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")));
 app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
 app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
@@ -39,10 +41,34 @@ app.get("/belowFifty", function(request, response){
 app.get("/aboveFifty", function(request, response){
 	aboveFifty(response);
 });
+
 // Calling the about page
 app.get("/about", function(request, response){
 	response.sendFile(path.join(__dirname + "/public/about.html"));
 });
+
+// Creating our own search parameters
+app.get("/products/search=:term", function(request, response){
+	// This where you can find the search term entered into the url bar using the search form.
+	var term = request.params.term;
+	console.log(request.params.term);
+	searchData(response, term);
+});
+
+app.get("/products/search=:term/instock=:instock", function(request, response){
+	var term = request.params.term;
+	var stock = request.params.instock;
+	console.log(request.params);
+	searchDataInStock(response, term, stock);
+})
+
+app.get("/products/search=:term/instock=:instock/maxprice=:maxprice", function(request, response){
+	var term = request.params.term;
+	var stock = request.params.instock;
+	var price = request.params.maxprice;
+	console.log(request.params);
+	searchAvailAndPrice(response, term, stock, price);
+})
 
 app.use(cors);
 
@@ -72,4 +98,63 @@ function aboveFifty(response){
 	});
 
 	response.json(costlier);
+}
+
+// Parameter for search
+function searchData(response, term){
+	term = term.toLowerCase();
+	var list = data.filter(function(item){
+		var name = item.name.toLowerCase();
+
+		if(name.indexOf(term) !== -1){
+			return item;
+		}
+	});
+
+	response.end(JSON.stringify(list));
+}
+
+// For item in stock
+function searchDataInStock(response, term, stock){
+	term = term.toLowerCase();
+
+	if(stock === "true"){
+		var avail = true;
+	} else if(stock === "false"){
+		var avail = false;
+	}
+
+	var list = data.filter(function(item){
+		var name = item.name.toLowerCase();
+
+		// This asks whether the search term is included in the json file
+		if( (name.indexOf(term) !== -1) && (item.inStock === avail) ){
+			console.log(item);
+			return item;
+		}
+	})
+
+	response.end(JSON.stringify(list));
+}
+
+function searchAvailAndPrice(response, term, stock, price){
+	term = term.toLowerCase();
+
+	if(stock === "true"){
+		var avail = true;
+	} else if(stock === "false"){
+		var avail = false;
+	}
+
+	var list = data.filter(function(item){
+		var name = item.name.toLowerCase();
+
+		// This asks whether the search term is included in the json file
+		if( (name.indexOf(term) !== -1) && (item.inStock === avail) && (item.price <= price) ){
+			console.log(item);
+			return item;
+		}
+	})
+
+	response.end(JSON.stringify(list));
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,46 @@
-// alert("Working!");
+$("#form").submit(function(event){
+
+	// prevent standard submission
+	event.preventDefault();
+
+	// Reetrieve search data from input form
+	var searchTerm = $("#search").val();
+	var inStock = $("#inStock").val();
+	var maxPrice = $("#maxPrice").val();
+
+	if(searchTerm.length === 0){
+		alert("Please enter something inside the search bar.");
+		return;
+	}
+
+	// Targeting html elements
+	var listContainer = $("#list-items")[0];
+	var tableContainer = $("#table-items")[0].childNodes[1];
+
+	// Empty the table using remove class
+	$(".appended-row").remove();
+
+	$.ajax({
+		url: "http://localhost:3000/products/search=" + searchTerm + "/instock=" + inStock + "/maxprice=" + maxPrice,
+		dataType:"json",
+		success: function(Data){
+			for (var i = 0; i < Data.length; i++) {
+
+				// Setting variables using json data
+				var itemName = Data[i].name;
+				var itemPrice = Data[i].price;
+				
+				if(Data[i].inStock === true){
+					var itemStock = "Yes";
+				} else if(Data[i].inStock === false){
+					var itemStock = "No";
+				}
+
+				// Appending information to table
+				tableContainer.innerHTML += "<tr class='appended-row'><td>" + itemName + "</td><td>" + itemStock + "</td><td>$" + itemPrice + ".00</td></tr>";
+			}
+		}, error: function(){
+			console.log("Error, server not responding!");
+		}
+	})
+});
